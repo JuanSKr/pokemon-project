d86ae4c0f76a34d7b479b661ca5112cc2fc12b1e
Juancg <mr.juancg@gmail.com>
2023-03-14 12:17:52 +0100

    Añadido diagrama E/R
    Se ha creado la carpeta "bd" y se ha añadido el diagrama de entidad relación.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
     }
 }
