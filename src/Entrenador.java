bb72463d711252721c31adbbe063724d5b656223
MiguelMoya89 <127111987+MiguelMoya89@users.noreply.github.com>
2023-03-27 23:02:56 +0200

    Crear Clases
    Creacion de clases

---
+++ src/Entrenador.java
@@ -0,0 +1,9 @@
+
+public class Entrenador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        
 
     }
 }
