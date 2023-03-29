cb05501df2236b50afb909d97954d3bb4c982a15
Juancg <mr.juancg@gmail.com>
2023-03-29 12:11:05 +0200

    Añadidas las librerías de JavaFX
    Se han añadido las librerías de JavaFX y las demás clases.

---
+++ src/main/java/com/example/fx_pack/Ataque.java
@@ -0,0 +1,15 @@
+/*
+POR HACER:
+1. Constructores
+2. Getters and Setters (consultar cuando hacer y cuando no)
+3. Métodos
+ */
+
+public class Ataque extends Movimiento {
+
+    protected int potencia;
+    protected Pokemon tipoMovimiento;
+
+}
+
+
---
+++ src/main/java/com/example/fx_pack/tipoPokemon.java
@@ -0,0 +1,14 @@
+/*
+POR HACER:
+1. Constructores
+2. Getters and Setters (consultar cuando hacer y cuando no)
+3. Métodos
+ */
+
+import java.util.LinkedList;
+
+public class tipoPokemon {
+
+    LinkedList<String> pokeTipo = new LinkedList<String>();
+
+}
