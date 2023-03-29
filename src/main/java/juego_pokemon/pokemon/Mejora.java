82d0289d4a004ee77f340779d443f27016a05f14
Juancg <mr.juancg@gmail.com>
2023-03-29 21:51:28 +0200

    Carpeta actualizada
    Se ha añadido todo a la carpeta y los constructores de movimiento y su herencia

--- src/main/java/juego_pokemon/pokemon/Mejora.java
+++ src/main/java/juego_pokemon/pokemon/Mejora.java
@@ -12,4 +12,7 @@ public class Mejora extends Movimiento {
     protected int turnos;
     protected String mejora;
 
+    public Mejora(int idMovimiento, Pokemon tipoMovimiento, String nombreMovimiento) {
+        super(idMovimiento, tipoMovimiento, nombreMovimiento);
+    }
 }
--- src/main/java/juego_pokemon/pokemon/Movimiento.java
+++ src/main/java/juego_pokemon/pokemon/Movimiento.java
@@ -8,9 +8,49 @@ POR HACER:
  */
 
 public abstract class Movimiento {
+
+    // Atributos de la clase
+
     protected int idMovimiento;
     protected Pokemon tipoMovimiento;
     protected String nombreMovimiento;
+
+    // Constructor con todos los parametros
+
+    public Movimiento(int idMovimiento, Pokemon tipoMovimiento, String nombreMovimiento) {
+        this.idMovimiento = idMovimiento;
+        this.tipoMovimiento = tipoMovimiento;
+        this.nombreMovimiento = nombreMovimiento;
+    }
+
+    // Metodos:
+
+    int patata = 5;
+
+
+    public int getIdMovimiento() {
+        return idMovimiento;
+    }
+
+    public void setIdMovimiento(int idMovimiento) {
+        this.idMovimiento = idMovimiento;
+    }
+
+    public Pokemon getTipoMovimiento() {
+        return tipoMovimiento;
+    }
+
+    public void setTipoMovimiento(Pokemon tipoMovimiento) {
+        this.tipoMovimiento = tipoMovimiento;
+    }
+
+    public String getNombreMovimiento() {
+        return nombreMovimiento;
+    }
+
+    public void setNombreMovimiento(String nombreMovimiento) {
+        this.nombreMovimiento = nombreMovimiento;
+    }
 }
 
-// Metodos:
+
