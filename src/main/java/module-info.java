e03aa05bdc6e0c23723811a76604779d31b4b4d2
MiguelMoya89 <127111987+MiguelMoya89@users.noreply.github.com>
2023-03-30 00:48:22 +0200

    Revert "Revert "cambios""
    This reverts commit f245df1a0b9c97fdb7a95e58bb9276c9c7bbe1c4.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,5 @@
 module juego_pokemon.pokemon {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires com.almasb.fxgl.all;
-
-    opens juego_pokemon.pokemon to javafx.fxml;
-    exports juego_pokemon.pokemon;
+    
+    opens src.main.java.juego_pokemon.pokemon to javafx.fxml;
+    exports src.main.java.juego_pokemon.pokemon;
 }
