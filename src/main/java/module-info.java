a212f8fc88ada4bffb1c3073f6f5ae455225715c
Juancg <mr.juancg@gmail.com>
2023-03-29 12:09:35 +0200

    JavaFX añadido.
    Se han añadido todas las librerías de JavaFX.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.fx_pack {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.fx_pack to javafx.fxml;
+    exports com.example.fx_pack;
+}
